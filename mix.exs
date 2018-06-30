b0667d37fa218cf72e0b4849d6efe6d3e2df4871
Maarten Faddegon <github@maartenfaddegon.nl>
2018-06-30 12:51:31 +0200

    ex meck 0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMeck.MixProject do
   def project do
     [
       app: :ex_meck,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       description: description(),
@@ -36,8 +36,8 @@ defmodule ExMeck.MixProject do
 
   defp package do
     [
-      name: "ExMeck",
-      files: ["lib/exmeck.ex", "mix.exs"],
+      name: "ex_meck",
+      files: ["lib/ex_meck.ex", "mix.exs"],
       maintainers: ["Maarten Faddegon"],
       licenses: ["MIT License"],
       links: %{"GitHub" => "https://github.com/MaartenFaddegon/ex_meck"}
